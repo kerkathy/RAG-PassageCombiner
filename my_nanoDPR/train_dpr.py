fix path setting in config ; correct t5 score calculation

my_nanoDPR/train_dpr.py
@@ -122,12 +122,19 @@ class QADataset(torch.utils.data.Dataset):
         query_inputs = self.ret_tokenizer([x[0] for x in samples], max_length=256, padding=True, truncation=True, return_tensors='pt')
         # collect doc_inputs from doc_embeddings
         doc_embeddings = torch.stack([x[3] for x in samples], dim=0)
-        prompt_ans_lm_inputs = self.lm_tokenizer(
-            [" ".join([x[0], x[1]]) for x in samples], 
-            [x[2] for x in samples],
-            max_length=256, padding=True, truncation=True, return_tensors='pt',
-            return_token_type_ids=True
-        )
+        prompt = [" ".join([x[0], x[1]]) for x in samples]
+        answer = [x[2] for x in samples]
+        if "t5" in self.lm_tokenizer.name_or_path:
+            # separate input_ids (send into encoder) and labels (send into decoder)
+            input_ids = self.lm_tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=512).input_ids
+            labels = self.lm_tokenizer(answer, return_tensors="pt", padding=True, truncation=True, max_length=512).input_ids
+            prompt_ans_lm_inputs = {"input_ids": input_ids, "labels": labels}
+        else:
+            prompt_ans_lm_inputs = self.lm_tokenizer(
+                prompt, answer,
+                max_length=256, padding=True, truncation=True, return_tensors='pt',
+                return_token_type_ids=True
+            )
 
         return {
             "query_inputs": query_inputs,
@@ -311,29 +318,26 @@ def main():
         args.lm_model, device=accelerator.device, model_parallelism=args.model_parallelism, cache_dir=args.cache_dir, auth_token=args.auth_token
     )
     model_max_length = lm_config.n_positions if hasattr(lm_config, "n_positions") else lm_config.max_position_embeddings
-    lm_tokenizer.pad_token = "[PAD]"
-    lm_tokenizer.padding_side = "left"
+    # only pad if model is gpt2
+    if "gpt2" in args.lm_model:
+        lm_tokenizer.pad_token = "[PAD]"
+        lm_tokenizer.padding_side = "left"
     logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
 
-    logger.info("...Loading data...")
-    train_data = json.load(open(args.train_file))
-
-    if args.data_size == "1/10":
-        train_data = train_data[:10000]
-    elif args.data_size == "debug":
-        train_data = train_data[:50]
-
-    if args.train_file == args.dev_file:
-        # random split train and dev data
-        random.shuffle(train_data)
-        dev_data = train_data[:len(train_data)//10]
-        train_data = train_data[len(train_data)//10:]
+    if args.data_size == "debug":
+        train_size, dev_size = 50, 10
+    elif args.data_size == "1/10":
+        train_size, dev_size = 10000, 1000
+    elif args.data_size == "full":
+        train_size, dev_size = 79168, 8757
     else:
-        dev_data = json.load(open(args.dev_file))
-        if args.data_size == "1/10":
-            dev_data = dev_data[:1000]
-        elif args.data_size == "debug":
-            dev_data = dev_data[:10]
+        raise ValueError(f"Invalid data_size: {args.data_size}")
+    args.train_file = args.train_file.replace(".json", f".size-{train_size}.json")
+    args.dev_file = args.dev_file.replace(".json", f".size-{dev_size}.json")
+
+    logger.info("...Loading data...")
+    train_data = json.load(open(os.path.join(args.train_dir, args.train_file)))
+    dev_data = json.load(open(os.path.join(args.dev_dir, args.dev_file)))
 
     logger.info(f"Size of train data: {len(train_data)}")
     logger.info(f"Size of dev data: {len(dev_data)}")
@@ -344,11 +348,15 @@ def main():
     logger.info(f"Size of train corpus: {len(train_corpus)}")
     logger.info(f"Size of dev corpus: {len(dev_corpus)}")
 
-    if os.path.exists(args.train_index_path) and os.path.exists(args.dev_index_path) and os.path.exists(args.empty_doc_embedding_path):
-        logger.info(f"...Loading index from {args.train_index_path} and {args.dev_index_path}...") 
-        train_doc_embeddings = torch.load(args.train_index_path)
-        dev_doc_embeddings = torch.load(args.dev_index_path)
-        empty_doc_embedding = torch.load(args.empty_doc_embedding_path)
+    train_index_path = os.path.join(args.index_dir, f"train_{train_size}.pt")
+    dev_index_path = os.path.join(args.index_dir, f"dev_{dev_size}.pt")
+    empty_doc_embedding_path = os.path.join(args.index_dir, "empty_doc.pt")
+
+    if os.path.exists(train_index_path) and os.path.exists(dev_index_path) and os.path.exists(empty_doc_embedding_path):
+        logger.info(f"...Loading index from {train_index_path} and {dev_index_path}...") 
+        train_doc_embeddings = torch.load(train_index_path)
+        dev_doc_embeddings = torch.load(dev_index_path)
+        empty_doc_embedding = torch.load(empty_doc_embedding_path)
         assert len(train_doc_embeddings) == len(train_corpus), f"len(train_doc_embeddings) ({len(train_doc_embeddings)}) != len(train_corpus), ({len(train_corpus)})"
         assert len(dev_doc_embeddings) == len(dev_corpus), f"len(dev_doc_embeddings) ({len(dev_doc_embeddings)}) != len(dev_corpus), ({len(dev_corpus)})"
     else:
@@ -481,44 +489,44 @@ def main():
             with accelerator.accumulate(query_encoder): # gradient accumulation
                 with accelerator.autocast():
                     logger.debug("...Sending batch to model...")
-                    logger.debug(f"batch['query_inputs']['input_ids']: {batch['query_inputs']['input_ids'].shape}")
-                    logger.debug(f"batch['doc_embeddings']: {batch['doc_embeddings'].shape}")
+                    # logger.debug(f"batch['query_inputs']['input_ids']: {batch['query_inputs']['input_ids'].shape}")
+                    # logger.debug(f"batch['doc_embeddings']: {batch['doc_embeddings'].shape}")
                     query_embedding = query_encoder(**batch['query_inputs']).pooler_output \
                         if "dpr" in args.query_encoder \
                         else query_encoder(**batch['query_inputs']).last_hidden_state[:,0,:]
-                    doc_embedding = batch["doc_embeddings"]
+                    # doc_embedding = batch["doc_embeddings"]
                     
-                    # logger.debug(f"query_embedding device: {query_embedding.device}; doc_embedding device: {doc_embedding.device}")
-                    logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
-                    # shape of both query_embedding and doc_embedding: [bs,n_dim]
-                    # where bs = n_comb * num_orig_question
+                    # # logger.debug(f"query_embedding device: {query_embedding.device}; doc_embedding device: {doc_embedding.device}")
+                    # logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
+                    # # shape of both query_embedding and doc_embedding: [bs,n_dim]
+                    # # where bs = n_comb * num_orig_question
                     single_device_query_num,_ = query_embedding.shape
-                    single_device_doc_num = doc_embedding.shape[0]
+                    # single_device_doc_num = doc_embedding.shape[0]
 
-                    logger.debug("...Waiting for everyone...")
-                    if accelerator.use_distributed:
-                        doc_list = [torch.zeros_like(doc_embedding) for _ in range(accelerator.num_processes)]
-                        dist.all_gather(tensor_list=doc_list, tensor=doc_embedding.contiguous())
-                        doc_list[dist.get_rank()] = doc_embedding
-                        doc_embedding = torch.cat(doc_list, dim=0)
+                    # logger.debug("...Waiting for everyone...")
+                    # if accelerator.use_distributed:
+                    #     doc_list = [torch.zeros_like(doc_embedding) for _ in range(accelerator.num_processes)]
+                    #     dist.all_gather(tensor_list=doc_list, tensor=doc_embedding.contiguous())
+                    #     doc_list[dist.get_rank()] = doc_embedding
+                    #     doc_embedding = torch.cat(doc_list, dim=0)
 
-                        query_list = [torch.zeros_like(query_embedding) for _ in range(accelerator.num_processes)]
-                        dist.all_gather(tensor_list=query_list, tensor=query_embedding.contiguous())
-                        query_list[dist.get_rank()] = query_embedding
-                        query_embedding = torch.cat(query_list, dim=0)
+                    #     query_list = [torch.zeros_like(query_embedding) for _ in range(accelerator.num_processes)]
+                    #     dist.all_gather(tensor_list=query_list, tensor=query_embedding.contiguous())
+                    #     query_list[dist.get_rank()] = query_embedding
+                    #     query_embedding = torch.cat(query_list, dim=0)
 
-                    logger.debug("...Calculating similarity score from DPR...")
-                    retriever_score = torch.sum(query_embedding * doc_embedding, dim=1)  # [bs]
+                    # logger.debug("...Calculating similarity score from DPR...")
+                    # retriever_score = torch.sum(query_embedding * doc_embedding, dim=1)  # [bs]
                     num_orig_question = single_device_query_num // sum([args.k ** i for i in range(args.max_round + 1)])
-                    retriever_score = retriever_score.reshape(num_orig_question, -1)
-                    logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
+                    # retriever_score = retriever_score.reshape(num_orig_question, -1)
+                    # logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
 
-                    del query_embedding, doc_embedding  
-                    logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
+                    # del query_embedding, doc_embedding  
+                    # logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
 
                     logger.debug(f"...Calculating loss from LM on {accelerator.device}...")
                     # very likely to OOM error here
-                    if "flan" in args.lm_model:
+                    if "t5" in args.lm_model:
                         lm_score = get_t5_lm_score(
                             **batch['prompt_ans_lm_inputs'],
                             model=language_model,
@@ -550,40 +558,40 @@ def main():
                     logger.debug(f"Finish compute loss. loss = {loss}")
                     logger.debug(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
 
-                accelerator.backward(loss)
+                # accelerator.backward(loss)
 
-                ## one optimization step
-                if accelerator.sync_gradients:
-                    progress_bar.update(1)
-                    progress_bar.set_postfix(loss=f"{loss:.4f}",lr=f"{lr_scheduler.get_last_lr()[0]:6f}")
-                    completed_steps += 1
-                    accelerator.clip_grad_norm_(query_encoder.parameters(), args.max_grad_norm)
-                    if not accelerator.optimizer_step_was_skipped:
-                        lr_scheduler.step()
-                    accelerator.log({"training_loss": loss}, step=completed_steps)
-                    accelerator.log({"lr": lr_scheduler.get_last_lr()[0]}, step=completed_steps)
+                # ## one optimization step
+                # if accelerator.sync_gradients:
+                #     progress_bar.update(1)
+                #     progress_bar.set_postfix(loss=f"{loss:.4f}",lr=f"{lr_scheduler.get_last_lr()[0]:6f}")
+                #     completed_steps += 1
+                #     accelerator.clip_grad_norm_(query_encoder.parameters(), args.max_grad_norm)
+                #     if not accelerator.optimizer_step_was_skipped:
+                #         lr_scheduler.step()
+                #     accelerator.log({"training_loss": loss}, step=completed_steps)
+                #     accelerator.log({"lr": lr_scheduler.get_last_lr()[0]}, step=completed_steps)
                     
-                    if completed_steps % EVAL_STEPS == 0 or completed_steps == MAX_TRAIN_STEPS:
-                        logger.info(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
-                        logger.info(f"\n...Evaluation...")
-                        steps_log_dir = os.path.join(LOG_DIR,f"step-{completed_steps}")
-                        if not os.path.exists(steps_log_dir):
-                            os.makedirs(steps_log_dir)
-                        loss = validate(query_encoder, language_model, dev_dataloader, lm_tokenizer, args, accelerator, model_max_length, steps_log_dir)
-                        query_encoder.train() # Make sure the model is back in training mode after validation
-                        accelerator.log({"eval":loss}, step=completed_steps)
-                        accelerator.wait_for_everyone()
-                        if accelerator.is_local_main_process:
-                            query_encoder_path = os.path.join(steps_log_dir, "query_encoder")
-                            ensure_directory_exists_for_file(query_encoder_path)
-                            query_encoder.save_pretrained(query_encoder_path)
-                            query_tokenizer.save_pretrained(query_encoder_path)
-                            logger.info(f"Checkpoint saved to {LOG_DIR}")
+                #     if completed_steps % EVAL_STEPS == 0 or completed_steps == MAX_TRAIN_STEPS:
+                #         logger.info(f"GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
+                #         logger.info(f"\n...Evaluation...")
+                #         steps_log_dir = os.path.join(LOG_DIR,f"step-{completed_steps}")
+                #         if not os.path.exists(steps_log_dir):
+                #             os.makedirs(steps_log_dir)
+                #         loss = validate(query_encoder, language_model, dev_dataloader, lm_tokenizer, args, accelerator, model_max_length, steps_log_dir)
+                #         query_encoder.train() # Make sure the model is back in training mode after validation
+                #         accelerator.log({"eval":loss}, step=completed_steps)
+                #         accelerator.wait_for_everyone()
+                #         if accelerator.is_local_main_process:
+                #             query_encoder_path = os.path.join(steps_log_dir, "query_encoder")
+                #             ensure_directory_exists_for_file(query_encoder_path)
+                #             query_encoder.save_pretrained(query_encoder_path)
+                #             query_tokenizer.save_pretrained(query_encoder_path)
+                #             logger.info(f"Checkpoint saved to {LOG_DIR}")
                             
-                        accelerator.wait_for_everyone()
+                #         accelerator.wait_for_everyone()
                 
-                optimizer.step()
-                optimizer.zero_grad()
+                # optimizer.step()
+                # optimizer.zero_grad()
                 logger.debug(f"Finish step {step}.\n GPU memory used: {torch.cuda.memory_allocated() / 1e6} MB")
     
     if accelerator.is_local_main_process:

my_nanoDPR/utils/lm_utils.py
@@ -84,19 +84,18 @@ def separate_prompt_answer(input_ids, token_type_ids, tokenizer, device, return_
     prompt_lengths = prompt_mask.sum(dim=1)
     # decode the prompt ids to check if the prompt is correct
     prompt_strs = tokenizer.batch_decode(prompt_input_ids.cpu(), skip_special_tokens=True)
-    print("Separated prompt: ", prompt_strs)
+    # print("Separated prompt: ", prompt_strs)
 
     if return_ans:
         ans_input_ids = input_ids * ~prompt_mask.to(device) # [bs, seq_len]
         # decode the answer ids to check if the answer is correct
         ans_strs = tokenizer.batch_decode(ans_input_ids.cpu(), skip_special_tokens=True)
-        print("Separated answer: ", ans_strs)
+        # print("Separated answer: ", ans_strs)
         return prompt_input_ids, prompt_lengths, ans_input_ids
 
     return prompt_input_ids, prompt_lengths
 
 
-# TODO check if arguments are correct when calling function
 def get_t5_lm_score(
     input_ids, labels,
     model, device, tokenizer,
@@ -116,6 +115,11 @@ def get_t5_lm_score(
     Then we feed the prompt to the encoder and the answer to the decoder.
     In that case, output of decoder is of has length of the answer, rather than the prompt_ans pair.
     """
+    # print("...In get_t5_lm_score...")
+    # print("input_ids.shape: ", input_ids.shape)
+    # print("labels.shape: ", labels.shape)
+    # print("llm_batch_size: ", llm_batch_size)
+
     num_too_long = 0
     if input_ids.shape[-1] > max_length - max_tokens_to_generate:
         num_too_long += 1
@@ -128,13 +132,19 @@ def get_t5_lm_score(
         input_ids_batch = input_ids_batch.to(device)
         labels_batch = labels_batch.to(device)
         with torch.no_grad():
-            logits_batch = model(input_ids=input_ids, labels=labels_batch).logits
+            logits_batch = model(input_ids=input_ids_batch, labels=labels_batch).logits
             eff_batch_size, seq_len = logits_batch.shape[:2]
-            ce_fn = CrossEntropyLoss(reduction="none", ignore_index=tokenizer.pad_token_id)
-            log_probs_batch = -ce_fn(logits_batch.view(-1, logits_batch.shape[-1]), labels_batch.view(-1))
+            ce_fn = CrossEntropyLoss(
+                reduction="none", ignore_index=tokenizer.pad_token_id)
+            log_probs_batch = -ce_fn(
+                logits_batch.view(-1, logits_batch.shape[-1]), labels_batch.view(-1))
+            
         log_probs.append(log_probs_batch.view(eff_batch_size, -1, seq_len).sum(dim=-1)) # [n_question_batch, n_comb]
-
-    return torch.cat(log_probs, dim=0).view(num_orig_question, -1) # [num_orig_question, n_comb]
+        # print("log_probs_batch.shape: ", log_probs[-1].shape)
+    log_probs = torch.cat(log_probs, dim=0)
+    # print("Concated log_probs.shape: ", log_probs.shape, "\n")
+    # print("log_probs: ", log_probs)
+    return log_probs.view(num_orig_question, -1) # [num_orig_question, n_comb]""
 
 
 def get_lm_score(
@@ -185,12 +195,10 @@ def get_lm_score(
     return all_outputs # [num_orig_question, n_comb]
 
 
-def get_batch_answer_from_model_output(outputs, tokenizer, prompt_lengths):
+def get_batch_answer_from_model_output(generation_strs, prompt_lengths):
     """
     For evaluation. Given a batch of model outputs, return the answer strings
     """
-    generation_strs = tokenizer.batch_decode(outputs.cpu(), skip_special_tokens=True)
-    
     answers = []
     for i, generation_str in enumerate(generation_strs):
         generation_str = generation_str[prompt_lengths[i]:]
@@ -207,26 +215,41 @@ def evaluate_dataset(
     num_too_long = 0
     all_predictions = []
 
-    input_ids = prompt_ans_lm_inputs["input_ids"].to(device)
-    token_type_ids = prompt_ans_lm_inputs["token_type_ids"].to(device) # token_type_ids = 0 for prompt, 1 for answer
-
-    # get decoded answers
-    answers = [tokenizer.decode(input_ids[i, token_type_ids[i] == 1], skip_special_tokens=True) for i in range(input_ids.shape[0])]
-
-    # extract prompt ids and prompt lengths from input_ids
-    all_prompt_input_ids, prompt_lengths = separate_prompt_answer(input_ids, token_type_ids, tokenizer, device, return_ans=False)
+    if "t5" in tokenizer.name_or_path:
+        answers = tokenizer.batch_decode(prompt_ans_lm_inputs["labels"], skip_special_tokens=True)
+        prompt_lengths = (prompt_ans_lm_inputs["input_ids"] != tokenizer.pad_token_id).sum(dim=1)
+        all_prompt_input_ids = prompt_ans_lm_inputs["input_ids"].to(device)
+    else:
+        input_ids = prompt_ans_lm_inputs["input_ids"].to(device)
+        token_type_ids = prompt_ans_lm_inputs["token_type_ids"].to(device) # token_type_ids = 0 for prompt, 1 for answer
+        # get decoded answers
+        answers = [tokenizer.decode(input_ids[i, token_type_ids[i] == 1], skip_special_tokens=True) for i in range(input_ids.shape[0])]
+        # extract prompt ids and prompt lengths from input_ids
+        all_prompt_input_ids, prompt_lengths = separate_prompt_answer(input_ids, token_type_ids, tokenizer, device, return_ans=False)
+        del prompt_ans_lm_inputs
 
     for prompt_input_ids in all_prompt_input_ids:
         if prompt_input_ids.shape[-1] > max_length - max_tokens_to_generate:
             num_too_long += 1
             prompt_input_ids = prompt_input_ids[..., -(max_length - max_tokens_to_generate):]
 
-    for input_ids_batch, prompt_length_batch in zip(all_prompt_input_ids.split(llm_batch_size), prompt_lengths.split(llm_batch_size)):
-        with torch.no_grad():
-            output_batch = model.generate(input_ids_batch, max_new_tokens=max_tokens_to_generate)
-            all_predictions.extend(get_batch_answer_from_model_output(output_batch, tokenizer, prompt_length_batch))
+    if "llama" in tokenizer.name_or_path:
+        # llama generation repeats the prompt, hence special handling
+        for input_ids_batch, prompt_length_batch in zip(all_prompt_input_ids.split(llm_batch_size), prompt_lengths.split(llm_batch_size)):
+            with torch.no_grad():
+                output_batch = model.generate(input_ids_batch, max_new_tokens=max_tokens_to_generate)
+            generation_strs = tokenizer.batch_decode(output_batch.cpu(), skip_special_tokens=True)
+            all_predictions.extend(get_batch_answer_from_model_output(generation_strs, prompt_length_batch))
+    else:
+        for input_ids_batch in all_prompt_input_ids.split(llm_batch_size):
+            with torch.no_grad():
+                output_batch = model.generate(input_ids_batch, max_new_tokens=max_tokens_to_generate)
+            generation_strs = tokenizer.batch_decode(output_batch.cpu(), skip_special_tokens=True)
+            all_predictions.extend(generation_strs)
 
     for prediction, answer in zip(all_predictions, answers):
+        # print(f"Prediction: {prediction}")
+        # print(f"Answer: {answer}", "\n")
         is_correct = exact_match(prediction, answer) # now we only have one ans per example
         # is_correct = any([exact_match(prediction, answer) for answer in answers])
         if is_correct: